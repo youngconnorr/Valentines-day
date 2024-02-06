249cc8e7cbceef0f4add0555fabe3ca8495aa63b
conryxng <youngconnor2004@gmail.com>
2024-02-06 13:12:46 -0800

    Added growing button and second page

--- script.js
+++ script.js
@@ -2,22 +2,35 @@ function translateButton() {
     const button = document.getElementById('translateButton');
   
     const translationX = Math.random() * 500;
-    const translationY = Math.random() * 300; 
+    const translationY = Math.random() * 200; 
     button.style.transform = `translate(${translationX}px, ${translationY}px)`;
   }
 
 function changePage() {
     document.body.innerHTML = '';
-
+    
     const photoContainer = document.createElement('div');
     photoContainer.id = 'photo-container';
     photoContainer.class = 'container center-vertically'
-  
+    
     const photo = document.createElement('img');
-    photo.src = 'cat-cat-thumbs-up.gif'; 
+    photo.src = 'cat-gif.gif'; 
+    photo.width = 200;
     photo.alt = 'Photo';
-  
+
+    const yes = document.createElement('div');
+    yes.textContent = '-----okay :)'
+    yes.className = ''
+    
     photoContainer.appendChild(photo);
-  
+    photoContainer.appendChild(yes);
+    
     document.body.appendChild(photoContainer);
+}
+
+let makeBigger = 1;
+function makeYesBigger() {
+    makeBigger = 1 + makeBigger;
+    const buttonYes = document.getElementById('buttonYes');
+    buttonYes.style.transform = `scale(${makeBigger})`;
 }
